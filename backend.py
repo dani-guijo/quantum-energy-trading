618791f0eeb97a6dc9aef5cd090fd8d8e82e78f1
Dani Guijo <dani_guijo@hotmail.com>
2024-01-28 22:45:45 +0100

    Update backend.py

--- backend.py
+++ backend.py
@@ -7,4 +7,4 @@ if __name__ == "__main__":
 
     df_length_per_hour = df.groupby('Hour').size()
     df_length_per_hour[df_length_per_hour<10] = 0
-    df_length_per_hour.describe(include='all')
+    print(df_length_per_hour.describe(include='all'))
