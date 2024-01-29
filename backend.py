6251380a30799010e5d22d68734e7897f1bc9535
Dani Guijo <dani_guijo@hotmail.com>
2024-01-29 21:13:07 +0100

    Update backend

--- backend.py
+++ backend.py
@@ -7,6 +7,4 @@ if __name__ == "__main__":
     
     df = market.data_generator()
 
-    df_length_per_hour = df.groupby('Hour').size()
-    df_length_per_hour[df_length_per_hour<10] = 0
-    market.summary(df_length_per_hour)
+    market.summary(df)
